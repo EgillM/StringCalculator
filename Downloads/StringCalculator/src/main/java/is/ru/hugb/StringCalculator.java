079880c3559f62e7e360d246ad2eddfe234ac230
Egill <egillhalldorsson@gmail.com>
2017-10-15 17:23:49 +0000

    step 4

--- Downloads/StringCalculator/src/main/java/is/ru/hugb/StringCalculator.java
+++ Downloads/StringCalculator/src/main/java/is/ru/hugb/StringCalculator.java
@@ -1,15 +1,28 @@
 package is.ru.hugb;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class StringCalculator {
 	public static int Add(String numbers) {
 		int a = 0;
-//		String values;
+		String negatives = "";
+		List negativeNumbers = new ArrayList();
 		if(numbers.equals(""))
 			return 0;
 		else {
 			String[] values = numbers.split("\\n|,");
 			for(int i = 0; i < values.length; i++) {
-				a += Integer.parseInt(values[i]);
+				int testNumber = Integer.parseInt(values[i]);
+				if(testNumber < 0){
+					negativeNumbers.add(testNumber);
+				}
+				else
+					a += testNumber;
+			}
+			if(negativeNumbers.size() > 0) {
+				throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
 			}
 		}
 		return a;
@@ -19,5 +32,6 @@ public class StringCalculator {
 		System.out.println(Add("1,2,3"));
 		System.out.println(Add("1,2,3,4,5,6\n7"));
 		System.out.println(Add("1"));
+		System.out.println(Add("1,2,3,-3,-2,-1,2"));
 	}
 }
